ff051cd156fcec38a3e7139925b137e95ed65f4a
LinkG <niri7879@gmail.com>
2025-01-05 21:29:16 +0100

    Skeleton

---
+++ sqp.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+#include<functional>
+
+using namespace std;
+
+//Takes a function 'f' and computes the unconstrained argmin of f
+float lagrange_newton(function<float(float)> f) {
+
+    return 0;
+}
+
+//Takes a function 'f' as argument and hopes to compute argmin of f subject to
+//conditions g_i and h_i.
+float sqp(function<float(float)> f, function<float(float)>* g, function<float(float)>* h) {
+
+    return 0;
+}
+
+int main() {
+    
+    return 0;
+}
